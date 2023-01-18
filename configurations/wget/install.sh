4d0188246e4b8edb10b111c3d0ace02e764ddac5
Gabe C <gabe@fracturedcode.net>
2023-01-17 23:25:07 -0700

    fix wget not install

--- configurations/wget/install.sh
+++ configurations/wget/install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mkdir -p $CONFIG/wget
-# https://stackoverflow.com/questions/47548271/disable-wget-history-tracking/54843736
-config_includes zsh && link_and_backup $(realpath wget-alias.zsh) $ZSH_CUSTOM/wget-alias.zsh
+config_includes zsh && link_and_backup configurations/wget/wget-alias.zsh $ZSH_CUSTOM/wget-alias.zsh
--- configurations/wget/wget-alias.zsh
+++ configurations/wget/wget-alias.zsh
@@ -1,2 +1,3 @@
 #!/bin/zsh
+# https://stackoverflow.com/questions/47548271/disable-wget-history-tracking/54843736
 alias wget='wget --hsts-file $XDG_CONFIG_HOME/wget/hsts'
