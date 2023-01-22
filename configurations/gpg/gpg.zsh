7bd02e08212e67f730fb029aafe7cacb8a39d6ed
Gabe C <gabe@fracturedcode.net>
2023-01-22 14:11:06 -0700

    gpg-relay bugfix (socat doesn't like forking shell functions

--- configurations/gpg/gpg.zsh
+++ configurations/gpg/gpg.zsh
@@ -1,19 +1,2 @@
 export GPG_TTY=$(tty)
 export GNUPGHOME=$XDG_CONFIG_HOME/gnupg
-gpg-relay() {
-    # What
-    # This is to pipe a windows gpg-agent into wsl.
-    #
-    # Setup
-    # Compile https://github.com/Lexicality/wsl-relay and place in your windows path
-    # Install socat
-    # On windows, your gpg-agent must be running. If it doesn't already start automatically, you can add
-    # a startup script to task scheduler or a systemd script in wsl to execute `gpg-connect-agent.exe /bye`
-    #
-    # Usage
-    # Usually, you'll want to fork this function: `gpg-relay &`
-    # You could create a gpg-relay daemon on wsl. I'm doing it this way
-    # because my weird use cases require both my gpg installs on wsl and win.
-    gpgconf --kill gpg-agent
-    exec socat $GNUPGHOME/S.gpg-agent,fork, EXEC:'wsl-relay.exe --input-closes --pipe-closes --gpg',nofork
-}
--- configurations/gpg/install.sh
+++ configurations/gpg/install.sh
@@ -5,3 +5,4 @@ backup_rm "$GNUPGHOME"
 mkdir -p $GNUPGHOME
 link_and_backup configurations/gpg/gpg.conf $GNUPGHOME/gpg.conf
 config_includes zsh && link_and_backup configurations/gpg/gpg.zsh $ZSH_CUSTOM/gpg.zsh
+link_and_backup configurations/gpg/gpg-relay $HOME/.local/bin/gpg-relay
