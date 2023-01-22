9e9e422b56ea5a1fa7b392c040c7f7b654ba8a3a
Gabe C <gabe@fracturedcode.net>
2023-01-22 14:31:19 -0700

    Revert "gpg-relay bugfix (socat doesn't like forking shell functions"
    This reverts commit 7bd02e08212e67f730fb029aafe7cacb8a39d6ed.

--- configurations/gpg/install.sh
+++ configurations/gpg/install.sh
@@ -5,4 +5,3 @@ backup_rm $HOME/.gnupg
 mkdir -p $GNUPGHOME
 link_and_backup configurations/gpg/gpg.conf $GNUPGHOME/gpg.conf
 config_includes zsh && link_and_backup configurations/gpg/gpg.zsh $ZSH_CUSTOM/gpg.zsh
-link_and_backup configurations/gpg/gpg-relay $HOME/.local/bin/gpg-relay
