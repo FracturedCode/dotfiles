9e6e7d67a81b6f83ec5a9909e60fb5d7b8a597ef
Gabe C <gabe@fracturedcode.net>
2023-01-22 14:15:10 -0700

    gpg explicitly backup and remove the default gnupg home

--- configurations/gpg/install.sh
+++ configurations/gpg/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # gpg on wsl
-backup_rm "$GNUPGHOME"
+backup_rm $HOME/.gnupg
 mkdir -p $GNUPGHOME
 link_and_backup configurations/gpg/gpg.conf $GNUPGHOME/gpg.conf
 config_includes zsh && link_and_backup configurations/gpg/gpg.zsh $ZSH_CUSTOM/gpg.zsh
