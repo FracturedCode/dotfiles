fee11f8062bee437b062ed5ba57847c407418523
Jesse C <jesse@fracturedcode.net>
2025-07-07 02:21:22 -0500

    move nano config to .config

--- configurations/nano/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p $CONFIG/nano
-backup_rm ~/.config/nano/nanorc # this is here for a reason
-link_and_backup $(realpath configurations/nano/nanorc) $CONFIG/nano/nanorc
-backup_rm ~/.nanorc
-link_and_backup $(realpath configurations/nano/syntax-highlighting) $CONFIG/nano/syntax-highlighting
-echo "include $CONFIG/nano/syntax-highlighting/*.nanorc" >> configurations/nano/nanorc
