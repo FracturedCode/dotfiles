51f9027c980ed0846908f1c38510583d35541c02
Jesse C <jesse@fracturedcode.net>
2025-07-07 02:33:02 -0500

    add powershell config

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,30 @@
+# XDG
+$Env:XDG_CONFIG_HOME = "$HOME/.config"
+$Env:XDG_DATA_HOME = "$HOME/.local/share"
+$Env:XDG_STATE_HOME = "$HOME/.local/state"
+
+# Variables
+
+# Path
+
+# Functions
+function Assert-IsInteractiveShell {
+    # Test each Arg for match of abbreviated '-NonInteractive' command.
+    $NonInteractive = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonI*' }
+
+    if ([Environment]::UserInteractive -and -not $NonInteractive) {
+        # We are in an interactive shell.
+        return $true
+    }
+
+    return $false
+}
+
+# Aliases
+function gs { git status }
+
+# Prompt
+if (Assert-IsInteractiveShell) {
+	hyfetch -b fastfetch
+	oh-my-posh init pwsh --config $PSScriptRoot/fracturedcode.omp.json | Invoke-Expression
+}
