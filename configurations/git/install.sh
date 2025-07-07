6dafcc68c3a2e6a9bf8b4c6be3aebb1443fae0d3
Jesse C <jesse@fracturedcode.net>
2025-07-07 02:24:05 -0500

    move git config to .config

--- configurations/git/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p $CONFIG/git
-link_and_backup $(realpath configurations/git/gitconfig) $CONFIG/git/config
-if [ -f ~/.gitconfig ]; then
-    backup_rm ~/.gitconfig
-fi
-# TODO add forcepull script
