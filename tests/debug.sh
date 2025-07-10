114d9d8a5ed984cf2f4499d35850064ffe7d8908
Jesse C <jesse@fracturedcode.net>
2025-07-10 00:20:47 -0700

    remove now unneeded tests

--- tests/debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-source common.sh
-
-SCRIPT="${2:-scripts/install.sh}"
-PLATFORM="${1:-alpine}"
-
-docker-run-test $SCRIPT $IMAGE_REPO/$PLATFORM:latest
--- tests/scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd .config/dotfiles
-
-sudo -E ./install.sh
-
-
-#TODO test assertion that symlinks were made correctly
