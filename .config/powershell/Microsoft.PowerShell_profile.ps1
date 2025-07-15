7f977d28b88ec16e86c8b8012e5eec2598514f9d
Jesse C <jesse@fracturedcode.net>
2025-07-15 00:42:02 -0700

    WIP

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -12,43 +12,44 @@ $Env:GPG_TTY = (tty)
 $Env:GNUPGHOME = "$XDG_CONFIG_HOME/gnupg"
 $Env:LANG="en_US.UTF-8"
 $Env:EDITOR="nano"
+$Env:LS_COLORS = (vivid generate molokai)
 
 # Path
 
 # Functions
 function Assert-IsInteractiveShell {
-    # Test each Arg for match of abbreviated '-NonInteractive' command.
-    $NonInteractive = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonI*' }
+	# Test each Arg for match of abbreviated '-NonInteractive' command.
+	$NonInteractive = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonI*' }
 
-    if ([Environment]::UserInteractive -and -not $NonInteractive) {
-        # We are in an interactive shell.
-        return $true
-    }
+	if ([Environment]::UserInteractive -and -not $NonInteractive) {
+		# We are in an interactive shell.
+		return $true
+	}
 
-    return $false
+	return $false
 }
 
 function Get-CustomFormatChildItem {
-    Get-ChildItem -Force $args | `
-        Select-Object UnixMode, User, Group, LastWriteTime, Size, @{ Name = "Name"; Expression = {Format-TerminalIcons $_} } | `
-        Format-Table
+	Get-ChildItem -Force $args | `
+		Select-Object UnixMode, User, Group, LastWriteTime, Size, @{ Name = "Name"; Expression = {Format-TerminalIcons $_} } | `
+		Format-Table
 }
 
 function Install-Modules {
-    param([string[]]$modules)
-    $modules | Foreach-Object {
-        if (-not (Get-Module -ListAvailable -Name $_)) {
-            Write-Host "$_ not found. Installing"
-            Install-Module -Name $_ -Force -Scope CurrentUser
-        }
-    }
+	param([string[]]$modules)
+	$modules | Foreach-Object {
+		if (-not (Get-Module -ListAvailable -Name $_)) {
+			Write-Host "$_ not found. Installing"
+			Install-Module -Name $_ -Force -Scope CurrentUser
+		}
+	}
 }
 
 # Aliases
 function gs { git status }
 function wget {
-    # https://stackoverflow.com/questions/47548271/disable-wget-history-tracking/54843736
-    wget --hsts-file $XDG_STATE_HOME/wget/hsts $args
+	# https://stackoverflow.com/questions/47548271/disable-wget-history-tracking/54843736
+	wget --hsts-file $XDG_STATE_HOME/wget/hsts $args
 }
 Set-Alias -Name top -Value btop
 Set-Alias -Name ls -Value Get-CustomFormatChildItem
@@ -59,13 +60,20 @@ Install-Modules @("PSReadLine", "CompletionPredictor", "DirectoryPredictor", "Te
 
 # Prompt
 if (Assert-IsInteractiveShell) {
-    
-    Import-Module PSReadLine
-    Import-Module CompletionPredictor
-    Import-Module DirectoryPredictor
-    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-    Set-PSReadLineOption -PredictionViewStyle ListView
+	
+	#Import-Module PSReadLine
+	#Import-Module CompletionPredictor
+	#Import-Module DirectoryPredictor
+	#Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+	#Set-PSReadLineOption -PredictionViewStyle ListView
 
 	hyfetch -b fastfetch
 	oh-my-posh init pwsh --config $PSScriptRoot/fracturedcode.omp.json | Invoke-Expression
+
+	$Env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
+	$Env:CARAPACE_MATCH = "1"
+	$Env:CARAPACE_TOOLTIP = "0"
+	Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+	Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+	carapace _carapace | Out-String | Invoke-Expression
 }
