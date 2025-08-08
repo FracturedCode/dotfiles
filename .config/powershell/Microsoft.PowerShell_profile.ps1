64a8c9afe6e80b9dbefbacc346d65e3bfbd3b985
Jesse C <jesse@fracturedcode.net>
2025-08-07 18:21:33 -0700

    move env vars in profile to env.ps1

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,5 @@
-# XDG
-$Env:XDG_CONFIG_HOME = "$HOME/.config"
-$Env:XDG_DATA_HOME = "$HOME/.local/share"
-$Env:XDG_STATE_HOME = "$HOME/.local/state"
-$Env:XDG_CACHE_HOME = "$HOME/.cache"
-
-$XDG_CONFIG_HOME = $Env:XDG_CONFIG_HOME
-$XDG_DATA_HOME = $Env:XDG_DATA_HOME
-$XDG_STATE_HOME = $Env:XDG_STATE_HOME
-$XDG_CACHE_HOME = $Env:XDG_CACHE_HOME
-
 # Variables
-$Env:GPG_TTY = (tty)
-$Env:GNUPGHOME = "$XDG_CONFIG_HOME/gnupg"
-$Env:LANG="en_US.UTF-8"
-$Env:EDITOR="nano"
-$Env:LS_COLORS = (vivid generate molokai)
-$Env:USER ??= (whoami)
-
-# Path
-$Env:PATH += ":$HOME/.local/bin"
+. $PSScriptRoot/env.ps1
 
 # Functions
 function Assert-IsInteractiveShell {
