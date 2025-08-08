2ecf402050ae78c21db0b468aeb2bc56a9c0f642
Jesse C <jesse@fracturedcode.net>
2025-08-07 23:52:51 -0700

    tool specific dotenv starting with gnupg

--- .config/powershell/env.ps1
+++ .config/powershell/env.ps1
@@ -22,8 +22,23 @@ $Env:LS_COLORS = (vivid generate molokai)
 $Env:BROWSER = "firefox"
 $Env:TERMINAL = "kitty"
 
-$Env:GPG_TTY = $(tty)
-$Env:GNUPGHOME = "$XDG_CONFIG_HOME/gnupg"
-
 # Info
 $Env:USER ??= (whoami)
+
+# Application-specific variables
+function Set-DotEnv {
+	param([string]$Path)
+	Get-Content $Path | Foreach-Object {
+		if ([String]::IsNullOrWhiteSpace($_) -or $_.TrimStart().StartsWith('#')) {
+			return
+		}
+		$equalsSign = $_.IndexOf('=')
+		$name = $_.Substring(0, $equalsSign)
+		$value = Invoke-Expression -Command $_.Substring($equalsSign + 1)
+		Set-Content env:\$name $value
+	}
+}
+
+Get-ChildItem -Hidden "$XDG_CONFIG_HOME/*/.env" | Foreach-Object {
+	Set-DotEnv -Path $_.FullName
+}
